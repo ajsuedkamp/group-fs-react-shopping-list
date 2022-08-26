21415fa738e6ad5402c5b76e72223ba05bc012c5
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-08-25 20:27:00 -0500

    axios GET complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,37 @@ import React from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 function App() {
+    const [groceryList, setGroceryList ] = useState([]);
+
+    useEffect(() => {
+        fetchGroceryList();
+    }, []);
+
+    const fetchGroceryList = () => {
+        axios({
+            method: 'GET',
+            url: '/grocery'
+        }).then(response => {
+            setGroceryList(response.data);
+        }).catch(error => {
+            console.log(error);
+            alert('Something went wrong!');
+        });
+    }
+    function addItem() {
+
+    }
     return (
         <div className="App">
             <Header />
+            <form onSubmit={addItem}></form>
             <main>
-                <p>Under Construction...</p>
+                <p>Under Construction...{JSON.stringify(groceryList)}</p>
             </main>
         </div>
     );
